fe893141eed7bbc76eb71b41ad478972d11db4d6
TRUONG Ngoc Tuan <tuan.truongngoc@pycogroup.com>
2019-11-15 17:22:26 +0700

    update using multiple config

--- packages/jetifier/index.js
+++ packages/jetifier/index.js
@@ -1,26 +1,39 @@
-const { fork, spawn } = require('child_process');
-const { join } = require('path');
-const { getClassesMapping, readDir2, chunk } = require('./src/utils');
+const { fork, spawn } = require("child_process");
+const { join } = require("path");
+const { getClassesMapping, readDir2, chunk } = require("./src/utils");
+const _ = require("lodash");
 
-const config = {
-  fileExtensionFilters: ["json", "js"],
-  dirGlob: "*.*"
-}
-
-const cpus = require('os').cpus().length;
-
+const cpus = require("os").cpus().length;
 const arg = process.argv.slice(2)[0];
-const mode = arg && ((arg === 'reverse') || (arg === '-r')) ? 'reverse' : 'forward';
-const SEARCH_DIR = 'node_modules';
+const mode = arg && (arg === "reverse" || arg === "-r") ? "reverse" : "forward";
 
-const classesMapping = getClassesMapping();
-const files = readDir2(SEARCH_DIR, config.dirGlob, config.fileExtensionFilters);
+const configs = [
+  {
+    SEARCH_DIR: "../../data/project-config",
+    fileExtensionFilters: ["json", "js"],
+    dirGlob: "*.*"
+  }
+];
 
-console.log(files);
-console.log(`Jetifier found ${files.length} file(s) to ${mode}-jetify. Using ${cpus} workers...`);
+function exec(config) {
+  const classesMapping = getClassesMapping();
+  const files = readDir2(
+    config.SEARCH_DIR,
+    config.dirGlob,
+    config.fileExtensionFilters
+  );
 
-for (const filesChunk of chunk(files, cpus)) {
-  const worker = fork(join(__dirname, 'src', 'worker.js'));
-  // worker.process.stdout.pipe(process.stdout);
-  worker.send({ filesChunk, classesMapping, mode });
+  console.log(files);
+  console.log(
+    `Jetifier found ${files.length} file(s) to ${mode}-jetify. Using ${cpus} workers...`
+  );
+
+  for (const filesChunk of chunk(files, cpus)) {
+    const worker = fork(join(__dirname, "src", "worker.js"));
+    worker.send({ filesChunk, classesMapping, mode });
+  }
 }
+
+_.each(configs, config => {
+  exec(config);
+})
